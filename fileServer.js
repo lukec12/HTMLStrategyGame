7335473bc836d09ccb23ddda2165340fcea3ab19
Lewis Ritz <lewisritz@lewisritz.com>
2015-04-01 18:17:38 -0400

    removed unnecessary things

--- fileServer.js
+++ fileServer.js
@@ -7,5 +7,3 @@ app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 8080);
 
 console.log("App listening on port 8080");
-//var d = new Date();
-//console.log("Date: "+d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDay()+" "+d.get);
